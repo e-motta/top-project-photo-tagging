31844ab4c1f73732f8854093dc69cc9a1741bfe0
Eduardo Motta de Moraes <edumoraes@live.com>
2022-12-28 09:08:23 -0300

    add AppCheck configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const app = initializeApp({
   apiKey: 'AIzaSyAs0DKIDTOvItk5kZrCdOEWT2kgwgKFl3c',
@@ -9,6 +10,11 @@ const app = initializeApp({
   messagingSenderId: '704820916168',
   appId: '1:704820916168:web:4d4658929c32549ce45afd',
   measurementId: 'G-B2S5CCM381',
+});
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LeQzbEjAAAAAO3AYUlEtNtl9a_zQ1Is2tD-DLTs'),
+  isTokenAutoRefreshEnabled: true,
 });
 
 export const firestore = getFirestore(app);
