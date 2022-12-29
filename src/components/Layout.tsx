f3cb1e159b8a0d18a3dd97a8014cb8a7777a151f
Eduardo Motta de Moraes <edumoraes@live.com>
2022-12-29 09:16:21 -0300

    limit page width to 1750px

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,14 @@ import Header from './Header';
 
 const Layout = () => {
   return (
-    <>
+    <div className="m-auto max-w-[1750px]">
       <Header />
       <main>
         <div>
           <Outlet />
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
